0a03be283d77a51e310915eafcf121e7f079a15a
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-04 07:58:43 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,9 +1,9 @@
 def my_select(nums)
   num = 0
   array = []
-   while num < nums.length
-    array << yield(nums[num])
-    num += 1
-   end
-   array
+    while num < nums.length
+     array << yield(nums[num])
+     num += 1
+    end
+ array
 end
